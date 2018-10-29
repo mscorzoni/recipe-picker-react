bf93f1bd0fdc927d6efa60f30216a47f2c9b9fc7
Marcos Scorzoni <mlessa@gmail.com>
2018-10-29 15:06:48 +0000

    fetching data from the server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+
+const API_KEY = '5bea2e86977e42ced540a80eb3e9779b';
+
 
 class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (event) => {
+    event.preventDefault();
+    // const recipeName = event.elmeents.recipeName.value;
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&page=5`);
+    
+    const data = await api_call.json();
+    console.log(data.recipes[0]);
+  }
+
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <header className="App-header">
+            <h1 className="App-title">Recipe Search</h1>
+          </header>
+        <Form getRecipe={this.getRecipe}/>
       </div>
+      
     );
   }
 }
 
 export default App;
+
