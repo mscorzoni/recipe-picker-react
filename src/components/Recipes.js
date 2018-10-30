8c05e87bac31fd4a0493d60e535b10a8d329e628
Marcos Scorzoni <mlessa@gmail.com>
2018-10-30 09:45:02 +0000

    style recipe pages

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 const recipes = (props) => (
-  <div>
-  {props.recipes.map((recipe) => {
-  return (
-    <div key={recipe.recipe_id}>
-      <img src={recipe.image_url} alt={recipe.title} />
-      <p>{recipe.title}</p>
+  <div className="container" >
+    <div className="row">
+      {props.recipes.map((recipe) => {
+        return (
+          <div className="col-md-4" key={recipe.title} style={{ marginBottom: '2rem'}}>
+            <div className="recipes__box">
+              <img 
+                src={recipe.image_url}
+                alt={recipe.title}
+                className="recipe__box-img"
+                />
+                <div className="recipe__text">
+                  <h5 className="recipes__title">
+                  {recipe.title.length < 20 ? recipe.title : `${recipe.title.substring(0, 15)}...` }
+                  </h5>
+                  <p className="recipes__subtitle">
+                    Publisher: <span>{recipe.publisher}</span>
+                  </p>
+                </div>
+                <button className="recipe_buttons">View Recipe</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  );
-  })}
   </div>
 );
 
